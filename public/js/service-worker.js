4917e492ea49c6e08babf3db3b30be6927c0a6c9
Kbentley8 <88289885+Kbentley8@users.noreply.github.com>
2021-11-05 15:08:38 -0400

    fixed mongoose connect

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -8,7 +8,7 @@ const FILES_TO_CACHE = [
   '/index.html',
   '/js/index.js',
   '/js/idb.js',
-  '/manifest.json',
+  '/manifest.webmanifest',
   '/css/styles.css',
   '/icons/icon-192x192.png',
   '/icons/icon-512x512.png'  
